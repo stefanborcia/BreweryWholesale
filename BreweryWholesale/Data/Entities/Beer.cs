5b28bd9d7a17ab58db59b59b9aeeb724bc4dafcf
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-08-16 22:28:37 +0200

    Create Entities and Properties

--- BreweryWholesale/Data/Entities/Beer.cs
+++ BreweryWholesale/Data/Entities/Beer.cs
@@ -1,6 +1,16 @@
-﻿namespace BreweryWholesale.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BreweryWholesale.Data.Entities
 {
     public class Beer
     {
+        [Key]
+        public int Id { get; set; }
+        public int BrewerId { get; set; }
+        public string? Name { get; set; }
+        public string? Alcohol { get; set; }
+        public double? Price { get; set; }
+        public virtual Brewery Brewery { get; set; }
+
     }
 }
--- BreweryWholesale/Data/Entities/Brewery.cs
+++ BreweryWholesale/Data/Entities/Brewery.cs
@@ -1,6 +1,12 @@
-﻿namespace BreweryWholesale.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BreweryWholesale.Data.Entities
 {
     public class Brewery
     {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public virtual List<Beer> Beers { get; set; }
     }
 }
