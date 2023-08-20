2141678c5733961b661d56e9792509ce9cac3ded
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-08-20 12:23:12 +0200

    create a HttpGet method GetWholesaler and model WholesalerDto to retrieve Wholesaler entity into a object

--- BreweryWholesale/Controllers/WholesalerController.cs
+++ BreweryWholesale/Controllers/WholesalerController.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Runtime.InteropServices;
+using System.Text;
 using BreweryWholesale.Data;
 using BreweryWholesale.Data.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BreweryWholesale.Controllers
 {
@@ -24,6 +26,28 @@ namespace BreweryWholesale.Controllers
              await _context.Wholesalers.AddAsync(wholesaler);
              await _context.SaveChangesAsync();
              return Created($"/{wholesaler.Id}",null);
+        }
+        [HttpGet]
+        [Route("{wholesalerId:int}")]
+        public async Task<ActionResult<Wholesaler>> GetWholesaler(int wholesalerId)
+        {
+            var wholesaler = await _context
+                .Wholesalers
+                .Include(w=>w.InventoryItems)
+                .FirstOrDefaultAsync(w => w.Id == wholesalerId);
+
+            if (wholesaler == null)
+            {
+                return NotFound($"Wholesaler with id{wholesalerId} doesn't exist.");
+            }
+
+            var stringBuilder = new StringBuilder();
+            foreach (var item in wholesaler.InventoryItems)
+            {
+                stringBuilder.AppendLine($"{wholesaler.Name} has {item.Stock} {item.BeerName} in stock.");
+            }
+
+            return Ok(stringBuilder.ToString());
         }
     }
 }
---
+++ BreweryWholesale/Models/WholesalerDto.cs
@@ -0,0 +1,22 @@
+﻿using BreweryWholesale.Data.Entities;
+
+namespace BreweryWholesale.Models
+{
+    public record WholesalerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static IEnumerable<WholesalerDto> WholesalerDtos(IEnumerable<Wholesaler> wholesalers)
+        {
+            return wholesalers.Select(w =>
+            {
+                return new WholesalerDto
+                {
+                    Id = w.Id,
+                    Name = w.Name,
+                };
+            });
+        }
+    }
+}
